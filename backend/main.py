201a88e8137c8a58593983614c448f572ee6df36
yishaq-official <yishaq.official@gmail.com>
2026-03-08 16:16:26 +0300

    added cors to recive frontend request

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.core.database import engine
 from app.models import tables
+from fastapi.middleware.cors import CORSMiddleware
 
 # Import all routers
 from app.api import sessions, users, movies
@@ -8,8 +9,15 @@ from app.api import sessions, users, movies
 tables.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Movie Night Agent API")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"], 
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
 
-# Register the routers
+
 app.include_router(sessions.router)
 app.include_router(users.router)
 app.include_router(movies.router) 
