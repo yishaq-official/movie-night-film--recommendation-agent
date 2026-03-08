68bbd0229bcdaf969af181cd17d660aedf98dd2d
yishaq-official <yishaq.official@gmail.com>
2026-03-08 15:07:49 +0300

    add youtube api to load thrillers

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,12 +1,15 @@
-# backend/app/core/config.py
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from the .env file
 load_dotenv()
 
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 TMDB_BASE_URL = "https://api.themoviedb.org/3"
 
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
+YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3"
+
 if not TMDB_API_KEY:
-    print("WARNING: TMDB_API_KEY is not set in the .env file.")
+    print("WARNING: TMDB_API_KEY is not set.")
+if not YOUTUBE_API_KEY:
+    print("WARNING: YOUTUBE_API_KEY is not set.")
